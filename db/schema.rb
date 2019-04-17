b228e6a03004a8ea394ec73987df86a329284cc7
DEVSINC399 <ertiza.ejaz@devsinc.com>
2019-04-17 15:46:24 +0500

    CRUD functionality of posts are compeleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_094118) do
+ActiveRecord::Schema.define(version: 2019_04_17_095158) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
   end
 
 end
