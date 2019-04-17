801c5dfc46d5fd19085e5207eea1ccb17a9f70e7
DEVSINC399 <ertiza.ejaz@devsinc.com>
2019-04-17 19:38:38 +0500

    Add and configure voting

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'simple_form', '~> 4.1'
 gem 'devise', '~> 4.6', '>= 4.6.2'
 gem 'paperclip', '~> 6.1'
 gem 'hirb', '~> 0.7.3'
+gem 'acts_as_votable', '~> 0.12.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :find_post, only: [:show, :edit, :update, :destroy]
+    before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
     before_action :authenticate_user!, except: [:index, :show]
     def index
         @posts = Post.all.order("created_at desc")
@@ -35,6 +35,16 @@ class PostsController < ApplicationController
     def destroy
         @post.destroy
         redirect_to root_path, notify: "Record saved successfully."
+    end
+
+    def upvote
+        @post.upvote by current_user
+        redirect_to root_path
+    end
+
+    def downvote
+        @post.downvote by current_user
+        redirect_to root_path
     end
 
     private
